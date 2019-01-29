06301cd8399233e85345ca65f3d72234a2a300a2
Jaafer Sheriff <jaafersheriff@gmail.com>
2019-01-29 17:24:08 +0000

    Proper water logic

--- gardener.py
+++ gardener.py
@@ -49,22 +49,37 @@ led = Component(37, "out")
 sensor = Component(8, "in")
 
 def start():
+    pump.turnOff()
+
     # 16 hours behind means 9:00 is 16:00 and 21:00 is 05:00
     schedule.every().day.at("17:00").do(led.turnOn)
     schedule.every().day.at("05:00").do(led.turnOff)
-    # TODO - check time to see where we're at and then turn on/off
-    pump.turnOff()
-    led.turnOn()
+    hour = datetime.datetime.now().hour
+    if hour >= 17 or hour < 5:
+        led.turnOn()
+    else:
+        led.turnOff()
+    drystart = None
+    drycounting = False
     while True:
+        if (sensor.read() == 0 and not drycounting):
+            drystart = datetime.datetime.now()
+            drycounting = True
+        if (drystart is not None and (datetime.datetime.now() - drystart).seconds > 2):
+            pump.turnOn()
+            time.sleep(1)
+            pump.turnOff()
+            drystart = None
+            drycounting = False
+            
         schedule.run_pending()
         time.sleep(1)
 
+# start infinite job on thread
 s = threading.Thread(target = start)
 s.daemon = True
 s.start()
 
-time.sleep(4)
- 
+# keep main thread alive 
 while True:
-    time.sleep(5)
-    print("next: " + str(schedule.next_run()))
+    time.sleep(1)
