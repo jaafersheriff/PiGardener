8e2a5f93e928fdb8297780fb12ea4751938a6e84
Jaafer Sheriff <jaafersheriff@gmail.com>
2019-06-11 22:07:19 -0700

    Better still script

--- still.py
+++ still.py
@@ -3,14 +3,17 @@ import time
 import datetime
 import math
 
+CAPTURE_TIME = 5
+
 def still():
-    command = "raspistill -o ./stills/pic" + str(int(math.floor(time.time()))) + ".jpg -th 0:0:0 -awb sun"
+    filename = "./stills/pic" + str(int(math.floor(time.time()))) + ".jpg"
+    command = "raspistill -o " + filename + " -th 0:0:0 -awb sun -n"
     subprocess.call(command, shell=True)
-    print "Still"
+    print "Still: " + filename
 
 while True:
     now = datetime.datetime.now()
-    if((now.time().hour >= 21 or now.time().hour < 9) and int(math.floor(time.time() % 3600)) == 3599):
+    if((now.time().hour >= 21 or now.time().hour < 9) and int(math.floor(time.time() % CAPTURE_TIME)) == CAPTURE_TIME - 1):
         still()
     time.sleep(1)
 
