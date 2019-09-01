a5a36e1517e17c2f28881987a15c5a96b2d0b1ea
Jaafer Sheriff <jaafersheriff@gmail.com>
2019-09-01 11:06:05 -0700

    enable/disable fan on timelapse capture

--- still.py
+++ still.py
@@ -3,6 +3,10 @@ import time
 import datetime
 import math
 
+import os
+import subprocess
+import sys
+
 CAPTURE_TIME = 3600
 
 def still():
@@ -14,6 +18,15 @@ def still():
 while True:
     now = datetime.datetime.now()
     if((now.time().hour >= 21 or now.time().hour < 9) and int(math.floor(time.time() % CAPTURE_TIME)) == CAPTURE_TIME - 1):
+        # turn off fan and wait for 5 seconds for it to stop spinning
+        os.system("echo raspberry | sudo -S ~/uhubctl/uhubctl -a 0 -p 1 -l 1-1.4")
+        time.sleep(5)
+
+        # capture frame 
         still()
+
+        # turn fan back on 
+        os.system("echo raspberry | sudo -S ~/uhubctl/uhubctl -a 1 -p 1 -l 1-1.4")
+
     time.sleep(1)
 
